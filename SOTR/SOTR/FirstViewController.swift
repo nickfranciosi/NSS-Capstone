5835d8293e1077718b5691ad1e1ff882a83df6bd
Nick Franciosi <nickfranciosi@gmail.com>
2015-03-18 20:41:47 -0500

    inital commit with project and workspace

---
+++ SOTR/SOTR/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SOTR
+//
+//  Created by Nick Franciosi on 3/18/15.
+//  Copyright (c) 2015 Nick Franciosi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
