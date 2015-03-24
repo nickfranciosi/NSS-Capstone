ad82628c5ce8047baf7cae74516d0e915dd593fa
Nick Franciosi <nickfranciosi@gmail.com>
2015-03-24 08:22:17 -0500

    updated featured cigar block

---
+++ SOTR/Eliptical/Eliptical/CustomCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.swift
+//  Eliptical
+//
+//  Created by Nick Franciosi on 3/23/15.
+//  Copyright (c) 2015 Nick Franciosi. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
