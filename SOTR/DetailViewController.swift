14d2b573192713e1ad6327faa7a8bb1ae2f6f179
Nick Franciosi <nickfranciosi@gmail.com>
2015-04-06 16:56:11 -0500

    added chart library

--- SOTR/DetailViewController.swift
+++ SOTR/DetailViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import QuartzCore
 
-class DetailViewController: UIViewController {
-
+class DetailViewController: UIViewController, LineChartDelegate {
+    
+    var label = UILabel()
+    var lineChart: LineChart!
+    
     @IBOutlet weak var bitterValue: UILabel!
     @IBOutlet weak var saltyValue: UILabel!
     @IBOutlet weak var sweetValue: UILabel!
@@ -28,9 +32,53 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         updateFlavorValues()
         updateName()
-        // Do any additional setup after loading the view.
+        var views: [String: AnyObject] = [:]
+        
+        label.text = "\(currentItem.name)"
+        label.setTranslatesAutoresizingMaskIntoConstraints(false)
+        label.textAlignment = NSTextAlignment.Center
+        self.view.addSubview(label)
+        views["label"] = label
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[label]-|", options: nil, metrics: nil, views: views))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-80-[label]", options: nil, metrics: nil, views: views))
+        
+        // simple arrays
+        var data: [CGFloat] = [CGFloat(currentItem.flavor.bitter), CGFloat(currentItem.flavor.salty), CGFloat(currentItem.flavor.sweet), CGFloat(currentItem.flavor.umami), CGFloat(currentItem.flavor.spicy)]
+        var data2: [CGFloat] = [1, 0, 3, 3, 0]
+        var maxXaxisSetter: [CGFloat] = [1,2,3,4,5]
+        
+        // simple line with custom x axis labels
+        var xLabels: [String] = ["Bitter", "Salty", "Sweet","Umami", "Spicy"]
+        var yLabels: [String] = ["0", "1", "2","3", "4", "5"]
+        
+        lineChart = LineChart()
+        lineChart.dots.visible = false
+        lineChart.animation.enabled = false
+        lineChart.area = true
+        lineChart.colors = [UIColor.clearColor(),UIColor(red: 252/255, green: 190/255, blue: 3/255, alpha: 0.5), UIColor(red: 100/255, green: 85/255, blue: 63/255, alpha: 0.5)]
+        lineChart.x.axis.visible = false
+        lineChart.y.axis.visible = false
+        lineChart.x.labels.visible = true
+        lineChart.x.grid.visible = false
+        lineChart.y.grid.visible = false
+        lineChart.x.grid.count = 6
+        lineChart.y.grid.count = 5
+        lineChart.x.labels.values = xLabels
+        lineChart.y.labels.values = yLabels
+        lineChart.y.labels.visible = true
+        lineChart.addLine(maxXaxisSetter)
+        lineChart.addLine(data)
+        lineChart.addLine(data2)
         
         
+        lineChart.setTranslatesAutoresizingMaskIntoConstraints(false)
+        lineChart.delegate = self
+        self.view.addSubview(lineChart)
+        views["chart"] = lineChart
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[chart]-|", options: nil, metrics: nil, views: views))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[label]-[chart(==200)]", options: nil, metrics: nil, views: views))
+        
+
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -46,19 +94,37 @@ class DetailViewController: UIViewController {
     }
     
     func updateFlavorValues(){
-        self.bitterValue.text! = "\(currentItem.flavor.bitter)"
-        self.saltyValue.text! = "\(currentItem.flavor.salty)"
-        self.sweetValue.text! = "\(currentItem.flavor.sweet)"
-        self.umamiValue.text! = "\(currentItem.flavor.umami)"
-        self.spicyValue.text! = "\(currentItem.flavor.spicy)"
+//        self.bitterValue.text! = "\(currentItem.flavor.bitter)"
+//        self.saltyValue.text! = "\(currentItem.flavor.salty)"
+//        self.sweetValue.text! = "\(currentItem.flavor.sweet)"
+//        self.umamiValue.text! = "\(currentItem.flavor.umami)"
+//        self.spicyValue.text! = "\(currentItem.flavor.spicy)"
 
         println(currentItem)
     }
     
     func updateName(){
-        self.nameLabel.text! = "\(currentItem.type!.rawValue.capitalizedString) name:"
-        self.itemName.text! = "\(currentItem.name)"
+//        self.nameLabel.text! = "\(currentItem.type!.rawValue.capitalizedString) name:"
+//        self.itemName.text! = "\(currentItem.name)"
         
+    }
+    
+    /**
+    * Line chart delegate method.
+    */
+    func didSelectDataPoint(x: CGFloat, yValues: Array<CGFloat>) {
+        label.text = "x: \(x)     y: \(yValues)"
+    }
+    
+    
+    
+    /**
+    * Redraw chart on device rotation.
+    */
+    override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
+        if let chart = lineChart {
+            chart.setNeedsDisplay()
+        }
     }
     
 }
