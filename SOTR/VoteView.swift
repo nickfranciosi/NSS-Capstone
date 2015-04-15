927f5f7261680b67d60a46061abe36787b9bb4bd
Nick Franciosi <nickfranciosi@gmail.com>
2015-04-15 16:53:27 -0500

    added vote view

---
+++ SOTR/VoteView.swift
@@ -0,0 +1,20 @@
+//
+//  VoteView.swift
+//  SOTR
+//
+//  Created by Nick Franciosi on 4/15/15.
+//  Copyright (c) 2015 Nick Franciosi. All rights reserved.
+//
+
+import UIKit
+
+class VoteView: UIView {
+
+    
+//    override func drawRect(rect: CGRect) {
+//        self.layer.cornerRadius = 30.0
+//    }
+    
+    
+
+}
